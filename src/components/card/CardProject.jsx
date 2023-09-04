d98a0d346f73e93eab9b741b9ced5f8e62c1ff0a
antony <gonzalescamposantony@gmail.com>
2023-09-04 09:44:26 -0500

    version-3.5

--- src/components/card/CardProject.jsx
+++ src/components/card/CardProject.jsx
@@ -27,7 +27,7 @@ const CardProject = ({ project }) => {
     <>
       <section onClick={handleDataSubmit} ref={ref} className={`grid items-center justify-center text-center grid-cols-2 w-[270px] h-[180px] bg-slate-200/10 rounded-md hover:w-[290px] hover:h-[200px] transition-all duration-500 ${inView ? 'opacity-100' : 'opacity-0'} cursor-pointer ${isHovered ? 'w-[290px] h-[200px]' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <div className='h-[160px] col-span-2 rounded-md'>
-          <img className='rounded-md shadow-md shadow-blue-500 w-full h-full object-fill' src='/public/img/e-commerce.PNG' alt=''/>
+          <img className='rounded-md shadow-md shadow-blue-500 w-full h-full object-fill' src='/img/e-commerce.PNG' alt=''/>
         </div>
         {/* <a href={project.demo} className='hover:font-semibold hover:text-blue-500 pt-1 h-[30px]'>Prueba</a>
         <a href={project.code} className='hover:font-semibold hover:text-blue-500 pt-1 h-[30px]'>Codigo</a>
